957174b5821416f0081d918f05b14ba907916149
Ishara Wijesundara <wijesundaraishara2001@gmail.com>
2024-01-20 14:08:16 +0530

    Find the area and the perimeter of a rectangle

---
+++ part 2/Activity2.py
@@ -0,0 +1,8 @@
+length = int(input("Enter the length of the rectangle : "))
+width = int(input("Enter the width of the rectangle : "))
+
+area = length * width
+perimeter = (2*length) + (width * 2)
+
+print ("Area = ", area)
+print ("Perimeter = ", perimeter)

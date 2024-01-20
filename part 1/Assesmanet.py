8c9ad9a34c17afa6381e49258f8c715bd776f8aa
Ishara Wijesundara <wijesundaraishara2001@gmail.com>
2024-01-20 13:46:03 +0530

    renamed:    1.py -> part 1/1.py 	renamed:    2.py -> part 1/2.py 	renamed:    3.py -> part 1/3.py 	renamed:    4.py -> part 1/4.py 	renamed:    5.py -> part 1/5.py 	new file:   part 1/Assesmanet.py 	new file:   part 2/Activity1.py

---
+++ part 1/Assesmanet.py
@@ -0,0 +1,17 @@
+# Lecture1 Part b
+
+#Method1
+print("Method1")
+print("Name : Ishara Wijesundara")
+print("School : Ku/ Maliyadeva Balika Vidyalaya\n")
+
+#Method2
+print("Method2")
+def printName_School():
+  name = "Ishara Wijesundara"
+  school = "Ku/ Maliyadeva Balika Vidyalaya"
+  
+  print(name)
+  print(school)
+
+printName_School()
---
+++ part 2/Activity1.py
@@ -0,0 +1,6 @@
+value1 = int(input("Enter value 1 "))
+value2 = int(input("Enter value 2 "))
+value3 = int(input("Enter value 3 "))
+
+print ("Maximum value is " ,(max(value1, value2, value3)))
+print ("Minimum value is " ,(min(value1, value2, value3)))

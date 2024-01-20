3b08db08fdb7a4ab8705988d7bfc48bc3670b916
Ishara Wijesundara <wijesundaraishara2001@gmail.com>
2024-01-20 14:15:05 +0530

    use of selection

---
+++ part 2/Activity3.py
@@ -0,0 +1,8 @@
+length = int(input("Enter the length of the rectangle : "))
+width = int(input("Enter the width of the rectangle : "))
+
+if length == width :
+    print ("It's a square")
+else:
+    print ("It's not a square")
+

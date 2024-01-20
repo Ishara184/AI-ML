0197080fc1f8cd0703c72d5cd30639cd60142a1e
Ishara Wijesundara <156680354+Ishara184@users.noreply.github.com>
2024-01-20 13:56:30 +0530

    Delete AI ML/part 1 directory

--- AI ML/part 1/1.py
+++
@@ -1,7 +0,0 @@
-#function
-
-def add(no1, no2, no3):
-    sum = no1 + no2 + no3
-    return sum
-
-print(add(10,56,90))
--- AI ML/part 1/2.py
+++
@@ -1,6 +0,0 @@
-#Sequence
-
-no1 = int(input("Enter first number: "))
-no2 = int(input("Enter first number: "))
-sum = no1 + no2
-print(sum)

021f027092d9b704691d40f3dda8f774686b09f4
Ishara Wijesundara <wijesundaraishara2001@gmail.com>
2024-01-24 11:32:47 +0530

    tuples

---
+++ part 3/tuple.py
@@ -0,0 +1,15 @@
+#use normal brackets
+#can add different data types
+#can't modify the data = imutable
+
+#create tuple
+t = ("apple", "banana", "berry", 45, 56.7, True)
+
+#print the tuple
+print(t)
+
+#print the data type
+print(type(t))
+
+#can extract element using index
+print (t[0]) 

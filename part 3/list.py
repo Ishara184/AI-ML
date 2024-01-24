5ee318554c9a24bb7c7752f6355b39eb7722171d
Ishara Wijesundara <wijesundaraishara2001@gmail.com>
2024-01-24 11:32:22 +0530

    list in python

---
+++ part 3/list.py
@@ -0,0 +1,17 @@
+#use square brackets
+#can add different data types
+#can modify the data = mutable
+
+#create list
+l = ["apple", "banana", "berry", 45, 56.7, True]
+
+#print the list
+print(l)
+
+#print the data type
+print(type(l))
+
+#can extract element using index
+print (l[0])
+
+l[0] = "mango"

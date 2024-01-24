4292237bf8805dd36758c0fdb5ba735d4704c8b3
Ishara Wijesundara <wijesundaraishara2001@gmail.com>
2024-01-24 11:32:38 +0530

    slicing collections

---
+++ part 3/slicing.py
@@ -0,0 +1,14 @@
+#the extracted elements will satiffy following conditions
+#   >= start
+#   < end
+#   increment step
+
+
+
+l = ["apple", "banana", "berry", 45, 56.7, True]
+
+print(l[0:2]) #0,1
+print(l[1:3]) #1,2
+print(l[4:]) #last two elements
+print(l[:5]) #print elements from first to 4
+print(l[0:5:2]) #index 0-5 expect one

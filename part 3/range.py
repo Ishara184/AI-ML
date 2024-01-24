b1015464c11f4fc8bea027173af471cfcd186f4a
Ishara Wijesundara <wijesundaraishara2001@gmail.com>
2024-01-24 11:41:52 +0530

    using range

---
+++ part 3/range.py
@@ -0,0 +1,16 @@
+for no in range(10):
+    print(no)
+
+print("\n")
+
+for no in range(10, 20):
+    print(no)
+
+print("\n")
+
+#           (start,end,stepValue)
+
+for no in range(10,20,2):
+    print(no)
+    
+print("\n")
